dfb4c0205257c7562faf233f73e1ce7052df007e
Fur1ouS <Fur1ouS@ya.ru>
2026-04-03 02:45:32 +0300

    add states and useeffect for get data

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./index.css";
 
-//Rus
-
-//БЕЗ ПОДСКАЗОК:
-//Создайте интерфейс для конвертации валют с загрузкой данных валют из API Frankfurter в state, динамическим отображением options в select, обработкой выбранных валют, ввода суммы, расчётом конвертации через асинхронную функцию с try/catch/finally, отображением результата в UI, проверкой, что сумма больше 0, и состояниями для загрузки и ошибок.
-
-//C ПОДСКАЗКАМИ:
-/*
-// 1 - Получите массив всех валют из API Frankfurter и запишите его в state.
-// 2 - Используя map, динамически создайте options внутри select.
 // 3 - Получите значения выбранных валют из обоих select и запишите их в state fromCurrency и toCurrency.
 // 4 - Создайте state для записи amount из input. Запишите данные из input в этот state.
 // 5 - Создайте вторую асинхронную функцию для получения значения конвертации двух валют. Запишите результат конвертации в новый state - convertedAmount. Покажите результат в интерфейсе.
 // 6 - Добавьте в обе функции блоки try/catch/finally. Создайте state для loading (true/false) и error ("Сообщение ошибки").
 // 7 - Внедрите логику отображения загрузки и ошибок в интерфейсе.
 // 8 - Добавьте проверку, чтобы amount был больше 0.
-*/
 
 //https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD
 
-const API_URL = "https://api.frankfurter.app/";
+const API_URL = "https://api.frankfurter.app";
 
 function App() {
+  const [currency, setCurrency] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState('')
+  const [toCurrency, setToCurrency] = useState('')
+  const [amount, setAmount] = useState('')
+
+  useEffect(() => {
+    async function currencyData()  {
+      try{
+        const resp = await fetch(API_URL + '/latest')
+        if(!resp.ok){
+          throw new Error("Error fetching currency data.");
+        }
+        const data = await resp.json();
+
+        setCurrency(Object.keys(data.rates));
+
+      }catch(err){
+        console.error(err);
+      }
+    }
+
+    currencyData()
+  },[])
+
+  console.log(fromCurrency,
+  toCurrency,
+  amount)
   return (
     <div className="app">
       <h1>Currency Exchange Calculator</h1>
@@ -31,13 +48,13 @@ function App() {
         <p className="error"></p>
 
         <div className="input-group">
-          <input type="number" placeholder="Amount" className="input-field" />
-          <select className="dropdown">
-            <option></option>
+          <input type="number" placeholder="Amount" className="input-field" onChange={(e)=> setAmount(e.target.value)}/>
+          <select className="dropdown" onChange={(e) => setFromCurrency(e.target.value)}>
+            {currency.map(cur => <option key={cur}>{cur}</option>)}
           </select>
           <span className="arrow">→</span>
-          <select className="dropdown">
-            <option></option>
+          <select className="dropdown" onChange={(e) => setToCurrency(e.target.value)}>
+            {currency.map(cur => <option key={cur}>{cur}</option>)}
           </select>
         </div>
         <button className="convert-button">Convert</button>
